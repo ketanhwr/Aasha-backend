8b289457c05738ebafd6d48109579a9014d4f985
Ketan Gupta <ketan19972010@gmail.com>
2017-02-04 19:52:33 +0530

    Adds Base Models and migrations

---
+++ database/migrations/2017_02_04_135539_create_hospitals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHospitalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hospitals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->text('details');
+            $table->double('gpsx', 15, 8);
+            $table->double('gpsy', 15, 8);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hospitals');
+    }
+}
---
+++ database/migrations/2017_02_04_140616_create_appointments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('patient_id')->unsigned();
+            $table->integer('hospital_id')->unsigned();
+            $table->integer('doctor_id')->unsigned();
+            $table->timestamp('time');
+            $table->text('description');
+            $table->text('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
